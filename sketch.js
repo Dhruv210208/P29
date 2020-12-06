8a8c8bcdd124fa6dcadd481fccb261fe7a869352
Dhruv210208 <72986486+Dhruv210208@users.noreply.github.com>
2020-12-06 21:22:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
+
+var engine, world;
+var box1;
+var gameState = "onSling";
+
+function setup(){
+    var canvas = createCanvas(1600,620);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(400,400,50,50);
+    box2 = new Box(450,490,50,50);
+    box3 = new Box(500,490,50,50);
+    box4 = new Box(550,490,50,50);
+    box5 = new Box(600,490,50,50);
+    box6 = new Box(500,400,50,50);
+    box7 = new Box(550,400,50,50);
+    box8 = new Box(450,400,50,50);
+    box9= new Box(500,400,50,50);
+
+
+ground1=new Ground(1200,490,300,20)
+ground2=new Ground(500,590,300,20)
+bird = new Bird(100,100,50,50);
+
+chain = new Chain(bird.body,{x:200,y:200});
+
+
+}
+
+function draw(){
+    background(239,123,118);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    bird.display();
+chain.display();
+
+    
+
+    ground1.display();
+    ground2.display();
+}
+
+function mouseDragged() {
+    if (gameState !== "launched") {
+        Matter.Body.setPosition(bird.body, { x: mouseX, y: mouseY });
+    }
+}
+
+
+function mouseReleased() {
+    chain.fly();
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+         chain.attach(bird.body);
+    }
+
+}
